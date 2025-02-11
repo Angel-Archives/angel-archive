869308d5b1f557d0f18e4cd1db6eee04c531b539
Mei Zhu <152559872+maggiez13@users.noreply.github.com>
2025-02-11 08:33:55 -0500

    included logo, nav bar done, links to sign up and log in pages, landing join button links to sign up, added user flows to sign up and log in forms

--- frontend/components/NavBarLanding.jsx
+++ frontend/components/NavBarLanding.jsx
@@ -1,30 +1,43 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
-import ApiIcon from '@mui/icons-material/Api';
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "@mui/material";
 
 export function NavBarLanding() {
+    const navigate = useNavigate();
+    const theme = useTheme(); 
+
+    const handleSignUp = () => {
+        navigate("/signup");
+    };
+
+    const handleLogin = () => {
+        navigate("/login");
+    };
+
     return (
         <AppBar position="sticky">
             <Toolbar 
                 sx={{ 
                     display: 'flex', 
                     justifyContent: 'space-between', 
-                    // padding: { sm: '0' }, 
                     height: '80px' 
                 }}
             >
-                <Box 
-                    display="flex" 
-                    alignItems="center"
-                >
-                    <ApiIcon sx={{ marginRight: "12px", fontSize: "3rem" }} />
+                <Box display="flex" alignItems="center">
+                    <img 
+                        src="../src/assets/AngelArchiveLogo.png" 
+                        alt="Angel Archive Logo" 
+                        style={{ width: "73px", height: "80px", marginRight: "12px", objectFit: "contain" }} 
+                    />
                     <Typography 
                         variant="h6" 
-                        sx={{ fontSize: "1.875rem", fontWeight: "bold" }}
+                        sx={{ fontSize: "1.875rem", fontWeight: "bold", display: "flex", alignItems: "center" }}
                     >
-                        NavBarLanding
+                        Angel Archive 
                     </Typography>
                 </Box>
+
                 <Box 
                     display="flex" 
                     alignItems="center"
@@ -37,26 +50,39 @@ export function NavBarLanding() {
                             height: '48px',
                             padding: '10px 20px',
                             marginLeft: '16px',
-                            width: 'auto'
+                            width: 'auto',
+                            // change colors for hover if want to -> prob should, not aesthetically pleasing right now 
+                            '&:hover': {
+                                backgroundColor: theme.palette.primary.light, 
+                                borderColor: theme.palette.primary.dark, 
+                            },
+                            width: '125px', 
                         }}
-                    >
-                        Log In
-                    </Button>
-                    <Button 
-                        color="inherit" 
-                        variant="outlined" 
-                        sx={{ 
-                            fontSize: '1.25rem',
-                            height: '48px',
-                            padding: '10px 20px',
-                            marginLeft: '16px',
-                            width: 'auto'
-                        }}
+                        onClick={handleSignUp}
                     >
                         Sign Up
                     </Button>
+                    <Button 
+                        color="inherit" 
+                        variant="outlined" 
+                        sx={{ 
+                            fontSize: '1.25rem',
+                            height: '48px',
+                            padding: '10px 20px',
+                            marginLeft: '16px',
+                            width: 'auto',
+                            // change colors for hover if want to -> prob should, not aesthetically pleasing right now 
+                            '&:hover': {
+                                backgroundColor: theme.palette.primary.light, 
+                                borderColor: theme.palette.primary.dark, 
+                            },
+                            width: '125px', 
+                        }}
+                        onClick={handleLogin}
+                    >
+                        Log In
+                    </Button>
                 </Box>
-
             </Toolbar>
         </AppBar>
     );
