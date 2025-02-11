64773da82beeca1ed4b7c751d801e021009df4dc
Mei Zhu <152559872+maggiez13@users.noreply.github.com>
2025-02-11 03:42:35 -0500

    finalized sign up form, unique username needed, uploaded images for background uses

--- frontend/src/utils/queries.js
+++ frontend/src/utils/queries.js
@@ -12,7 +12,6 @@ export async function fetchAngelsImages() {
     return data;  
 }
 
-// Create a new user in the "users" table after authentication
 export const createUserInDatabase = async (userData) => {
   const { data, error } = await supabase.from("users").insert([userData]);
   if (error) {
